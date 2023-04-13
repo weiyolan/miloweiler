fd33c5d051aa77def9dd040cbc3f9aabf70cee22
weiyolan <yolan.weiler@gmail.com>
2023-04-13 16:21:48 +0200

    portfolio and gallery

--- src/components/ProjectPicture.jsx
+++ src/components/ProjectPicture.jsx
@@ -9,12 +9,12 @@ import useDimensions from '@/utils/useDimensions'
 
 export default function ProjectPicture({ images, mainPictureHeight, setMainPictureWidth, visibleItem, handleVisibility, nextVisibility, prevVisibility, index }) {
   const { locale, width } = useAppContext()
-
+  // const [clicked,setClicked] = useState(false)
   let projectPictureContainerRef = useRef(null)
-  let {width:projectPictureContainerWidth} = useDimensions(projectPictureContainerRef)
+  let { width: projectPictureContainerWidth } = useDimensions(projectPictureContainerRef)
   // console.log(projectPictureContainerWidth)
 
-  useEffect(()=>{
+  useEffect(() => {
     setMainPictureWidth(projectPictureContainerWidth)
   })
 
@@ -25,15 +25,15 @@ export default function ProjectPicture({ images, mainPictureHeight, setMainPictu
 
       {images.map((image, i) => <MainPicture mainPictureHeight={mainPictureHeight} index={i} key={i} visible={visibleItem[i]} image={image} alt={`Project image ${i}`} prevVisibility={prevVisibility} nextVisibility={nextVisibility} />)}
 
-      <div className=' absolute left-0 bottom-0 flex items-center cursor-pointer  w-1/2 lg:relative lg:w-[37%] h-[100%] lg:pt-1 z-[1]'
+      <div className=' group absolute left-0 bottom-0 flex items-center cursor-pointer w-1/2 lg:relative lg:w-[37%] h-[100%] lg:pt-1 z-[1]'
         // onClick={() => { if (visible) { prevVisibility() } }}>
         onClick={prevVisibility}>
-          
-      </div>
-      <div className='absolute right-0 bottom-0 flex items-center cursor-pointer w-1/2 lg:relative lg:w-[37%] h-[100%] lg:pt-1 z-[1]'
+        {width > 1024 && <AiFillCaretLeft className={`opacity-0 group-hover:opacity-30 transition duration-300 w-10 h-10 `}/>}
+             </div>
+      <div className='group absolute right-0 bottom-0 flex items-center justify-end cursor-pointer w-1/2 lg:relative lg:w-[37%] h-[100%] lg:pt-1 z-[1]'
         // onClick={() => { if (visible) { nextVisibility() } }}>
         onClick={nextVisibility}>
-
+        {width > 1024 && <AiFillCaretRight className={`opacity-0 group-hover:opacity-30 transition duration-300 w-10 h-10 `}/>}
       </div>
 
       {/* <PictureIndicator setPosition={setIndicatorPosition} handleVisibility={handleVisibility} visibleItem={visibleItem} /> */}
@@ -46,8 +46,8 @@ export default function ProjectPicture({ images, mainPictureHeight, setMainPictu
 
 function MainPicture({ visible, image, alt, prevVisibility, nextVisibility, index, mainPictureHeight }) {
   let mainPicRef = useRef(null)
-  let { width:pictureWidth, height, paddingLeft } = useDimensions(mainPicRef, { padding: true })
-  let {width} = useAppContext()
+  let { width: pictureWidth, height, paddingLeft } = useDimensions(mainPicRef, { padding: true })
+  let { width } = useAppContext()
   let ar = image.asset.metadata.dimensions.aspectRatio
 
   // index===6 && console.log(mainPictureHeight, (width-2*paddingLeft)/ar - 2)
@@ -57,7 +57,7 @@ function MainPicture({ visible, image, alt, prevVisibility, nextVisibility, inde
       className={`absolute flex justify-center items-center max-w-[1500px] max-h-[80vh] w-full rounded-lg left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 px-2 pt-4 xs:px-4 md:px-32 lg:px-56 lg:m-0 `}>
       <SanityImage
         className={`mainPicture-${index} will-change-transform opacity-0 invisible shadow-2xl shadow-[var(--my-shadow-color)] `}
-        sizes='(max-width: 700px) 95vw, 50vw'
+        sizes='(max-width: 700px) 95vw, 60vw'
         style={{ objectFit: 'contain', transform: 'translate3d(0,0,0)' }}
         image={image}
         alt={alt} />
