1f11b4264deab4ed18d5c16f394504846ddc3830
weiyolan <yolan.weiler@gmail.com>
2023-11-05 13:08:21 +0100

    introe Ended off

--- src/components/line/Page0Logo.jsx
+++ src/components/line/Page0Logo.jsx
@@ -123,16 +123,16 @@ export default function Page0Logo({ className, style, introAnimationTl }) {
 
   }, [introAnimationTl])
 
-  useEffect(() => {
-    function scrollHandler() {
-      if (scrolled.current > 0.165 && !introEnded) {
-        // gsap.getById('introAnimation')?.progress(1);
-        setIntroEnded(true);
-      }
-    }
-    document.addEventListener('scroll', scrollHandler)
-    return () => document.removeEventListener('scroll', scrollHandler)
-  }, [introEnded])
+  // useEffect(() => {
+  //   function scrollHandler() {
+  //     if (scrolled.current > 0.165 && !introEnded) {
+  //       // gsap.getById('introAnimation')?.progress(1);
+  //       setIntroEnded(true);
+  //     }
+  //   }
+  //   document.addEventListener('scroll', scrollHandler)
+  //   return () => document.removeEventListener('scroll', scrollHandler)
+  // }, [introEnded])
 
   let colorPrimary = "#FFF5EA";
 
