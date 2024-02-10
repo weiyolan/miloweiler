2a7f6d6d70c15fc58e2540083e3159e8a1244ea6
weiyolan <yolan.weiler@gmail.com>
2024-02-11 00:34:14 +0100

    again

--- src/pages/(gallery)/([slug] copy).js
+++ src/pages/(gallery)/([slug] copy).js
@@ -4,136 +4,139 @@ import { useAppContext } from "@/utils/appContext";
 import { PageWrapper } from "@/utils/pageContext";
 import Head from "next/head";
 // import Image from "next/image";
-import React, { useState, useEffect, useRef } from "react"
-import client from '../../../lib/sanity'
+import React, { useState, useEffect, useRef } from "react";
+import client from "../../../lib/sanity";
 // import { IoClose} from 'react-icons/io5'
-import { IoArrowBack } from 'react-icons/io5'
+import { IoArrowBack } from "react-icons/io5";
 import Link from "next/link";
 import ProjectPictures from "@/components/ProjectPictures";
 import ProjectPicture from "@/components/ProjectPicture";
-import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 import Layout from "@/components/Layout";
 // import ProjectDescriptionTop from "@/components/ProjectDescriptionTop";
-import { gsap } from 'gsap/dist/gsap'
+import { gsap } from "gsap/dist/gsap";
 import useLocalStorage from "@/utils/useLocalStorage";
-import { Observer } from 'gsap/dist/Observer'
+import { Observer } from "gsap/dist/Observer";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import PictureIndicator from "@/components/PictureIndicator";
 import Line from "@/components/Line";
 
-gsap.registerPlugin(Observer, ScrollToPlugin, ScrollTrigger)
+gsap.registerPlugin(Observer, ScrollToPlugin, ScrollTrigger);
 
 export default function Project({ project, slug, slugs }) {
   // console.log(project)
-  let [animating, setAnimating] = useState(false)
-  let darkMode = true
-  const { width, height, locale } = useAppContext()
+  let [animating, setAnimating] = useState(false);
+  let darkMode = true;
+  const { width, height, locale } = useAppContext();
   // let [loaded, setLoaded] = useState(false)
-  let [visibleItem, setVisibleItem] = useLocalStorage(`${slug}-visibleItem`, initiateVisibility())
+  let [visibleItem, setVisibleItem] = useLocalStorage(`${slug}-visibleItem`, initiateVisibility());
   // let [clicked, setClicked] = useState(false)
-  let [descriptionOpen, setDescriptionOpen] = useState(false)
-  let [indicatorPosition, setIndicatorPosition] = useState(null)
-  let [descriptionPosition, setDescriptionPosition] = useState(null)
-  let [mainPictureHeight, setMainPictureHeight] = useState(null)
-  let [mainPictureWidth, setMainPictureWidth] = useState(null)
-  const ctx = useRef(gsap.context(() => { }));
+  let [descriptionOpen, setDescriptionOpen] = useState(false);
+  let [indicatorPosition, setIndicatorPosition] = useState(null);
+  let [descriptionPosition, setDescriptionPosition] = useState(null);
+  let [mainPictureHeight, setMainPictureHeight] = useState(null);
+  let [mainPictureWidth, setMainPictureWidth] = useState(null);
+  const ctx = useRef(gsap.context(() => {}));
 
   // let [activeIndex, setActiveIndex] = useLocalStorage(`${slug}-activeIndex`, 0)
 
   useEffect(() => {
     // console.log(descriptionPosition)
     // console.log(indicatorPosition)
-    setMainPictureHeight(indicatorPosition?.top - descriptionPosition?.bottom)
+    setMainPictureHeight(indicatorPosition?.top - descriptionPosition?.bottom);
     // console.log('height: ' + (descriptionPosition - indicatorPosition))
+  }, [indicatorPosition, descriptionPosition]);
 
-  }, [indicatorPosition, descriptionPosition])
-
-
-  let palette = project.mainImage.image.asset.metadata.palette
+  let palette = project.mainImage.image.asset.metadata.palette;
   // let palette = Object.keys(project.mainImage.image.asset.metadata.palette).map((color,i)=>color.background);
   // {console.log(palette)}
-  let tl = useRef(null)
+  let tl = useRef(null);
 
   useEffect(() => {
-    setDescriptionOpen(false)
-    firstLoad()
+    setDescriptionOpen(false);
+    firstLoad();
     // console.log(visibleItem)
-  }, [visibleItem])
-
+  }, [visibleItem]);
 
   useEffect(() => {
     let observer = Observer.create({
-      target: window,         // can be any element (selector text is fine)
+      target: window, // can be any element (selector text is fine)
       ignore: ".project-pictures, .project-grid, .imageFill",
-      type: "touch, scroll, pointer",    // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
+      type: "touch, scroll, pointer", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
       preventDefault: false,
       onRight: () => {
         // console.log('right/prev');
         // setAnimating(true)
-        prevVisibility()
+        prevVisibility();
       },
       onLeft: () => {
         // console.log('left/next');
         // setAnimating(true)
-        nextVisibility()
+        nextVisibility();
       },
       lockAxis: true,
-    })
-    return () => { observer.disable() }
-  }, [visibleItem, animating, descriptionOpen])
+    });
+    return () => {
+      observer.disable();
+    };
+  }, [visibleItem, animating, descriptionOpen]);
 
   // console.log(animating)
 
   function initiateVisibility() {
-    let visibility = new Array(project.otherImages.length + 1).fill(false)
-    visibility[0] = true
-    return visibility
+    let visibility = new Array(project.otherImages.length + 1).fill(false);
+    visibility[0] = true;
+    return visibility;
   }
 
   function firstLoad() {
     if (visibleItem !== null) {
-      let activeIndex = visibleItem.indexOf(true)
+      let activeIndex = visibleItem.indexOf(true);
       gsap.to(`.mainPicture-${activeIndex}`, {
         autoAlpha: 1,
-      })
+      });
     }
   }
 
   function vanish(index1, index2, direction) {
     let xAmount = 30;
     let scaleAmount = 0.95;
-    gsap.killTweensOf(`.mainPicture-${index1}`)
-    gsap.killTweensOf(`.mainPicture-${index2}`)
+    gsap.killTweensOf(`.mainPicture-${index1}`);
+    gsap.killTweensOf(`.mainPicture-${index2}`);
     // tl.current
     // , ' appeared: ', index2, ' to: ', direction
     // { onComplete: () => { console.log('vanished: ', index1, ' appeared: ', index2, ' to: ', direction) } }
-    let tl = gsap.timeline({ autoRemoveChildren: true, onComplete: () => setAnimating(false) })
+    let tl = gsap
+      .timeline({ autoRemoveChildren: true, onComplete: () => setAnimating(false) })
       .set(`.mainPicture-${index2}`, {
-        x: () => direction === 'left' ? `${xAmount}` : `-${xAmount}`,
+        x: () => (direction === "left" ? `${xAmount}` : `-${xAmount}`),
         scale: scaleAmount,
         borderRadius: 0,
         // autoAlpha: 0,
       })
-      .to(`.mainPicture-${index1}`,
-        {
-          x: () => direction === 'left' ? `-=${xAmount}` : `+=${xAmount}`,
-          scale: scaleAmount,
-          autoAlpha: 0,
-          borderRadius: 0,
-          ease: 'expo.out',
-          // ease:'power4.out',
-          duration: 0.7,
-        })
-      .to(`.mainPicture-${index2}`, {
-        x: 0,
-        scale: 1,
-        autoAlpha: 1,
-        borderRadius: 5,
-        ease: 'expo.out',
+      .to(`.mainPicture-${index1}`, {
+        x: () => (direction === "left" ? `-=${xAmount}` : `+=${xAmount}`),
+        scale: scaleAmount,
+        autoAlpha: 0,
+        borderRadius: 0,
+        ease: "expo.out",
         // ease:'power4.out',
         duration: 0.7,
-      }, '<+=0.1')
+      })
+      .to(
+        `.mainPicture-${index2}`,
+        {
+          x: 0,
+          scale: 1,
+          autoAlpha: 1,
+          borderRadius: 5,
+          ease: "expo.out",
+          // ease:'power4.out',
+          duration: 0.7,
+        },
+        "<+=0.1"
+      );
     // .set(`.mainPicture-${index1}`,
     //   {
     //     x: 0,
@@ -148,29 +151,28 @@ export default function Project({ project, slug, slugs }) {
 
   useEffect(() => {
     ctx.current.add(() => {
-      gsap.to('.project-pictures', {
+      gsap.to(".project-pictures", {
         // x: selected === id ? 200 : 0,
         scrollTo:
           width < 1024
-            ? { x: `#pictureThumb${visibleItem?.indexOf(true)}`, offsetX: width < 350 ? ((width - 80) / 2) : ((width - 112) / 2) }
+            ? { x: `#pictureThumb${visibleItem?.indexOf(true)}`, offsetX: width < 350 ? (width - 80) / 2 : (width - 112) / 2 }
             : { y: `#pictureThumb${visibleItem?.indexOf(true)}`, offsetY: (height - 167) / 2 },
-        ease: 'power1.inout',
-        duration: 0.7
+        ease: "power1.inout",
+        duration: 0.7,
         // duration: width < 1024 ? 0.7 : 1,
         // ease: 'expo.inout',
       });
     });
   }, [visibleItem]);
 
-
   function handleVisibility(nextItem, direction) {
     // console.log(visibleItem)
     let currentItem = visibleItem.indexOf(true);
     let newVisibility = new Array(visibleItem.length).fill(false);
     newVisibility[nextItem] = true;
-    setVisibleItem(newVisibility)
+    setVisibleItem(newVisibility);
     if (nextItem !== currentItem) {
-      vanish(currentItem, nextItem, direction || (currentItem > nextItem ? 'right' : 'left'))
+      vanish(currentItem, nextItem, direction || (currentItem > nextItem ? "right" : "left"));
     }
   }
 
@@ -179,7 +181,7 @@ export default function Project({ project, slug, slugs }) {
   //   let newVisibility = new Array(qualities.length).fill(false);
   //   newVisibility[i] = newVal;
   //   setVisibleItem(newVisibility)
-  // } 
+  // }
   // else if (newVal === false) {
   //   randomVisibility(visibleItem)
   // }
@@ -195,7 +197,6 @@ export default function Project({ project, slug, slugs }) {
   //   handleVisibility(true, newIndex)
   // }
 
-
   function nextVisibility() {
     // console.log(visibleItem)
 
@@ -209,7 +210,7 @@ export default function Project({ project, slug, slugs }) {
     } else {
       let nextItem = currentItem === visibleItem.length - 1 ? 0 : currentItem + 1;
       // handleVisibility(true, nextItem)
-      handleVisibility(nextItem, 'left')
+      handleVisibility(nextItem, "left");
       // vanish(currentItem, nextItem, 'left')
       // console.log(currentItem, nextItem)
     }
@@ -225,7 +226,7 @@ export default function Project({ project, slug, slugs }) {
       // handleVisibility(true, 0)
     } else {
       let nextItem = currentItem === 0 ? visibleItem.length - 1 : currentItem - 1;
-      handleVisibility(nextItem, 'right')
+      handleVisibility(nextItem, "right");
       // vanish(nextItem, 'right')
       // vanishToLeft(currentItem, nextItem)
 
@@ -236,13 +237,13 @@ export default function Project({ project, slug, slugs }) {
   function prevSlug() {
     let currentItem = slugs.indexOf(slug);
     let nextItem = currentItem === 0 ? slugs.length - 1 : currentItem - 1;
-    return slugs[nextItem]
+    return slugs[nextItem];
     // console.log(slugs[nextItem])
   }
   function nextSlug() {
     let currentItem = slugs.indexOf(slug);
     let nextItem = currentItem === slugs.length - 1 ? 0 : currentItem + 1;
-    return slugs[nextItem]
+    return slugs[nextItem];
     // console.log(slugs[nextItem])
   }
 
@@ -382,31 +383,35 @@ export default function Project({ project, slug, slugs }) {
 export async function getStaticPaths({ locales }) {
   const projects = await client.fetch(`*[_type == "project"]{slug}`);
 
-  const slugs = projects.map((project) => project.slug.current)
+  const slugs = projects.map((project) => project.slug.current);
 
-  const paths = slugs.map((slug) =>
-    locales.map((locale) => ({
-      params: { slug: slug },
-      locale
-    })))
-    .flat(); // to avoid nested arrays 
+  const paths = slugs
+    .map((slug) =>
+      locales.map((locale) => ({
+        params: { slug: slug },
+        locale,
+      }))
+    )
+    .flat(); // to avoid nested arrays
 
   // let paths = [{params:{slug:'project-1'}}]
   return {
     paths,
     fallback: false, // can also be true or 'blocking'
-  }
+  };
 }
 
 // `getStaticPaths` requires using `getStaticProps`
 export async function getStaticProps({ params }) {
   // const project = await client.fetch(`*[_type == "project" && slug.current == "${params.slug}"][0]`);
-  const project = await client.fetch(`*[_type == "project" && slug.current == "${params.slug}"][0]{...,mainImage{alt,image{asset->{url,metadata}, ...asset{_ref}}},otherImages[]{_key,_type, asset->{url,metadata}, ...asset{_ref}}}`);
+  const project = await client.fetch(
+    `*[_type == "project" && slug.current == "${params.slug}"][0]{...,mainImage{alt,image{asset->{url,metadata}, ...asset{_ref}}},otherImages[]{_key,_type, asset->{url,metadata}, ...asset{_ref}}}`
+  );
   // *[_type == "project" ][0]{...,mainImage{alt,image{asset->{_ref,_type,url,metadata}}},otherImages[]{_key,_type,asset->{_ref,_type,url,metadata}}}
 
   const projectSlugs = await client.fetch(`*[_type == "project"]|order(date){slug}`);
   const slugNames = projectSlugs.map((projectSlug) => projectSlug.slug.current);
   return {
     props: { key: params.slug, project, slug: params.slug, slugs: slugNames },
-  }
+  };
 }
